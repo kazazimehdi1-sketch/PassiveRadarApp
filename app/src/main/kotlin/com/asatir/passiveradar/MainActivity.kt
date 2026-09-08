5372d99594fa56200a3d8343d95d5f6cfb33040f
kazazimehdi1-sketch <kazazimehdi1@gmail.com>
2026-09-08 07:11:05 +0330

    MainActivity.kt - اضافه شد

---
+++ app/src/main/kotlin/com/asatir/passiveradar/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.asatir.passiveradar
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PassiveRadarAppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MainScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    Text("برنامه فلزیاب منفعل")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreview() {
+    PassiveRadarAppTheme {
+        MainScreen()
+    }
+}
+
+@Composable
+fun PassiveRadarAppTheme(content: @Composable () -> Unit) {
+    MaterialTheme {
+        content()
+    }
+}
